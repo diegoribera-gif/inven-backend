ac7e0f8bb05238c670dd96157554cc78be59206a
Diego A. Ribera <diegoriberarodriguez@gmail.com>
2025-09-01 14:37:23 -0400

    Mover INVEN y requirements.txt a la raíz

--- INVEN/PRODUCT/API/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from PRODUCT.API.views import ProductoViewSet
-
-router = DefaultRouter()
-router.register('producto', ProductoViewSet, basename='producto')
-urlpatterns = router.urls
-
